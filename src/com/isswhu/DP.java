70c3778a0db6b6877d4bfbc7fc9ec234507a090b
zehaowei <314403865@qq.com>
2020-07-11 08:55:13 +0800

    [7-10] 3 problems

--- src/com/isswhu/DP.java
+++ src/com/isswhu/DP.java
@@ -1,6 +1,121 @@
 package com.isswhu;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class DP {
+
+    // 91 decode ways
+    public int numDecodings(String s) {
+        char[] chars = s.toCharArray();
+        int[] dp = new int[chars.length];
+        if (chars[0] > '0')
+            dp[0] = 1;
+        else
+            return 0;
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] > '0')
+                dp[i] = dp[i-1];
+            boolean yes = false;
+            if (chars[i-1] == '1' || (chars[i-1] == '2'&& chars[i] <= '6'))
+                yes = true;
+            if (i-2 >= 0 && chars[i-1] > '0' && yes) {
+                dp[i] += dp[i-2];
+            } else if (i-2 == -1 && yes) {
+                dp[i] += 1;
+            }
+        }
+        return dp[chars.length-1];
+    }
+
+    // 95 unique binary search trees 2
+    static class Solution {
+        static class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+            TreeNode() {}
+            TreeNode(int val) { this.val = val; }
+            TreeNode(int val, TreeNode left, TreeNode right) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public List<TreeNode> generateTrees(int n) {
+            if (n == 0)
+                return new LinkedList<>();
+            else {
+                List<TreeNode>[][] dp = new List[n+1][n+1];
+                return produce(1, n, dp);
+            }
+        }
+        public List<TreeNode> produce(int i, int j, List<TreeNode>[][] dp) {
+            if (i > j) {
+                return null;
+            }
+            if (dp[i][j] != null)
+                return dp[i][j];
+            if (i == j) {
+                TreeNode node = new TreeNode(i);
+                List<TreeNode> l = new LinkedList<>();
+                l.add(node);
+                dp[i][j] = l;
+                return l;
+            }
+            List<TreeNode> list = new LinkedList<>();
+            for (int k = i; k <= j; k++) {
+                List<TreeNode> left = produce(i, k-1, dp);
+                List<TreeNode> right = produce(k+1, j, dp);
+                if (left == null) {
+                    for (TreeNode nd : right) {
+                        TreeNode n = new TreeNode(k, null, nd);
+                        list.add(n);
+                    }
+                } else if (right == null) {
+                    for (TreeNode nd : left) {
+                        TreeNode n = new TreeNode(k, nd, null);
+                        list.add(n);
+                    }
+                } else {
+                    for (TreeNode nd_left : left) {
+                        for (TreeNode nd_right : right) {
+                            TreeNode n = new TreeNode(k, nd_left, nd_right);
+                            list.add(n);
+                        }
+                    }
+                }
+            }
+            dp[i][j] = list;
+            return list;
+        }
+    }
+
+    // 120 triangle
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int m = triangle.size();
+        if (m == 0)
+            return 0;
+        int n = triangle.get(m-1).size();
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 1; i < m+1; i++) {
+            for (int j = 1; j < n+1; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return dp(1, 1, triangle, dp, m);
+    }
+
+    public int dp(int i, int j, List<List<Integer>> triangle, int[][] dp, int last) {
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (i == last)
+            return triangle.get(i-1).get(j-1);
+        dp[i][j] = Math.min(dp(i+1, j, triangle, dp, last), dp(i+1, j+1, triangle, dp, last));
+        dp[i][j] += triangle.get(i-1).get(j-1);
+        return dp[i][j];
+    }
+
     // 221 Maximal Square
     public int maximalSquare(char[][] matrix) {
         int m = matrix.length;
