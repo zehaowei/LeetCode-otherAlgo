fbac20288e430e6306f1941f72f789ebdc86accb
zehaowei <314403865@qq.com>
2020-07-21 10:11:12 +0800

    [7-20] two problem

--- src/com/isswhu/DP.java
+++ src/com/isswhu/DP.java
@@ -154,7 +154,33 @@ public class DP {
 
     // 87 Scramble String
     public boolean isScramble(String s1, String s2) {
-
+        if (s1.length() != s2.length())
+            return false;
+        if (s1.length() == 0)
+            return true;
+        boolean[][][] dp = new boolean[s1.length()][s1.length()][s1.length()+1];
+        for (int i = 0; i < s1.length(); i++) {
+            for (int j = 0; j < s1.length(); j++) {
+                if (s1.charAt(i) == s2.charAt(j))
+                    dp[i][j][1] = true;
+            }
+        }
+        for (int l = 2; l <= s1.length(); l++) {
+            for (int i = 0; i <= s1.length()-l; i++) {
+                for (int j = 0; j <= s1.length()-l; j++) {
+                    boolean re = false;
+                    for (int w = 1; w <= l-1; w++) {
+                        if(dp[i][j][w] && dp[i+w][j+w][l-w]
+                                || dp[i][j+l-w][w] && dp[i+w][j][l-w]) {
+                            re = true;
+                            break;
+                        }
+                    }
+                    dp[i][j][l] = re;
+                }
+            }
+        }
+        return dp[0][0][s1.length()];
     }
 
     // 91 decode ways
@@ -241,6 +267,34 @@ public class DP {
             dp[i][j] = list;
             return list;
         }
+    }
+
+    // 97 Interleaving String
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length()+s2.length() != s3.length())
+            return false;
+        boolean[][] dp = new boolean[s1.length()+1][s2.length()+1];
+        dp[0][0] = true;
+        for (int i = 1; i <= s1.length(); i++) {
+            if(s1.substring(0,i).equals(s3.substring(0,i)))
+                dp[i][0] = true;
+        }
+        for (int j = 1; j <= s2.length(); j++) {
+            if(s2.substring(0,j).equals(s3.substring(0,j)))
+                dp[0][j] = true;
+        }
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                dp[i][j] = dp[i-1][j] && s1.charAt(i-1) == s3.charAt(i+j+1)
+                        || dp[i][j-1] && s2.charAt(j-1) == s3.charAt(i+j+1);
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+
+    // 115 Distinct Subsequences
+    public int numDistinct(String s, String t) {
+
     }
 
     // 120 triangle
--- src/com/isswhu/Main.java
+++ src/com/isswhu/Main.java
@@ -4,9 +4,5 @@ public class Main {
     static int[] dp;
 
     public static void main(String[] args) {
-        char[][] matrix = {{'1','0','1','0','0'},
-                {'1','0','1','1','1'},
-                {'1','1','1','1','1'},
-                {'1','0','0','1','0'}};
     }
 }
