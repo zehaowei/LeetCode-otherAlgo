9bd6a43ce060377cedffa6c8558b04295ce9b022
zehaowei <314403865@qq.com>
2020-07-22 15:47:09 +0800

    1 problem

--- src/com/isswhu/DP.java
+++ src/com/isswhu/DP.java
@@ -294,7 +294,21 @@ public class DP {
 
     // 115 Distinct Subsequences
     public int numDistinct(String s, String t) {
-
+        int[][] dp = new int[s.length()+1][t.length()+1];
+        for (int i = 0; i <= s.length(); i++)
+            dp[i][0] = 1;
+        for (int j = 1; j <= t.length(); j++)
+            dp[0][j] = 0;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= t.length(); j++) {
+                if (s.charAt(i-1) == t.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[s.length()][t.length()];
     }
 
     // 120 triangle
@@ -320,6 +334,11 @@ public class DP {
         dp[i][j] = Math.min(dp(i+1, j, triangle, dp, last), dp(i+1, j+1, triangle, dp, last));
         dp[i][j] += triangle.get(i-1).get(j-1);
         return dp[i][j];
+    }
+
+    // 123 Best Time to Buy and Sell Stock III
+    public int maxProfit(int[] prices) {
+
     }
 
     // 139 word break
