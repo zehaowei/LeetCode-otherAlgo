3a581b3966b53dcd5806d9eb7c3904e271b6afa7
zehaowei <314403865@qq.com>
2020-07-30 17:53:11 +0800

    dungeon game

--- src/com/isswhu/DP.java
+++ src/com/isswhu/DP.java
@@ -604,6 +604,31 @@ public class DP {
         return maxnum;
     }
 
+    // 174 Dungeon Game
+    public int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length;
+        if (m == 0)
+            return 1;
+        int n = dungeon[0].length;
+        int[][] dp = new int[m+1][n+1];
+        dp[m-1][n] = 1;
+        dp[m][n-1] = 1;
+        for (int i = 0; i <= m-2; i++)
+            dp[i][n] = Integer.MAX_VALUE;
+        for (int j = 0; j <= n-2; j++)
+            dp[m][j] = Integer.MAX_VALUE;
+        for (int i = m-1; i >= 0; i--) {
+            for (int j = n-1; j >= 0; j--) {
+                int require = Math.min(dp[i][j+1], dp[i+1][j]);
+                if (require - dungeon[i][j] > 0)
+                    dp[i][j] = require - dungeon[i][j];
+                else
+                    dp[i][j] = 1;
+            }
+        }
+        return dp[0][0];
+    }
+
     // 198 house rob 1
     public int rob1(int[] nums) {
         if (nums.length == 0)
