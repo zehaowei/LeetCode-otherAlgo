3ef7800f5de67daa3ccd6c83ecc9003f484ad6dc
zehaowei <314403865@qq.com>
2020-07-30 17:52:23 +0800

    2 problem

--- src/com/isswhu/BasicDataStructure.java
+++ src/com/isswhu/BasicDataStructure.java
@@ -5,7 +5,22 @@ import java.util.Stack;
 public class BasicDataStructure {
     // 48 Rotate Image
     public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
 
+        for (int j = 0; j < n/2; j++) {
+            for (int i = 0; i < n; i++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
     }
 
     // 84 Largest Rectangle in Histogram
