c3dc89cc2a092bddce36791f16dd6e2ca4eba6b9
zehaowei <314403865@qq.com>
2020-10-31 23:50:40 +0100

    [10-31] three

--- src/com/isswhu/BinarySearch.java
+++ src/com/isswhu/BinarySearch.java
@@ -184,6 +184,21 @@ public class BinarySearch {
 
     // 162. Find Peak Element
     public int findPeakElement(int[] nums) {
-
+        int n = nums.length;
+        if (n == 1)
+            return 0;
+        int l = 0, h = n-1;
+        while (l <= h) {
+            int mid = l + (h-l)/2;
+            if ((mid-1 == -1 || nums[mid-1] < nums[mid]) && (mid+1 == n || nums[mid] > nums[mid+1]))
+                return mid;
+            else if (mid-1 != -1 && nums[mid-1] > nums[mid])
+                h = mid-1;
+            else
+                l = mid+1;
+        }
+        return -1;
     }
+
+
 }
--- src/com/isswhu/LCOF.java
+++ src/com/isswhu/LCOF.java
@@ -1,6 +1,8 @@
 package com.isswhu;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Stack;
 
 public class LCOF {
     // 03. 数组中重复的数字
@@ -36,6 +38,71 @@ public class LCOF {
 
     // 05. 替换空格
     public String replaceSpace(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ')
+                count++;
+        }
+        char[] str = new char[s.length()+count*2];
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                str[i + count*2] = s.charAt(i);
+            } else {
+                count--;
+                str[i+count*2] = '%';
+                str[i+1+count*2] = '2';
+                str[i+2+count*2] = '0';
+            }
+        }
+        return new String(str);
+    }
+
+    // 06. 从尾到头打印链表
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public int[] reversePrint(ListNode head) {
+        Stack<Integer> stk = new Stack<>();
+        while (head != null) {
+            stk.push(head.val);
+            head = head.next;
+        }
+        int[] arr = new int[stk.size()];
+        int i = 0;
+        while (!stk.empty()) {
+            arr[i++] = stk.pop();
+        }
+        return arr;
+    }
+
+    // 07. 重建二叉树
+     static class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode(int x) { val = x; }
+     }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0)
+            return null;
+        else if (preorder.length == 1)
+            return new TreeNode(preorder[0]);
+        int root = preorder[0], cut = -1;
+        for (int i = 0; i < inorder.length; i++) {
+            if (root == inorder[i]) {
+                cut = i;
+                break;
+            }
+        }
 
     }
+
+    TreeNode build(int[] preorder, int l1, int h1, int[] inorder, int l2, int h2) {
+
+    }
+
 }
