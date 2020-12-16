659286a634253a2e644892839f73bffe0fcbef45
zehaowei <314403865@qq.com>
2020-12-16 19:40:32 +0800

    [12-16] 5 problem

--- src/com/isswhu/DFS.java
+++ src/com/isswhu/DFS.java
@@ -1,5 +1,215 @@
 package com.isswhu;
 
-public class DFS {
+import java.util.*;
 
+public class DFS {
+    // 109. 有序链表转换二叉搜索树
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null)
+            return null;
+        else if (head.next == null)
+            return new TreeNode(head.val);
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            pre = cur;
+            cur = cur.next;
+        }
+        return buildTree(head, pre);
+    }
+
+    TreeNode buildTree(ListNode head, ListNode tail) {
+        if (head == tail)
+            return new TreeNode(head.val);
+        else if (tail == null)
+            return null;
+        ListNode fast = head, slow = head, pre = null;
+        while (fast != tail) {
+            if (fast.next == tail)
+                break;
+            fast = fast.next.next;
+            pre = slow;
+            slow = slow.next;
+        }
+        TreeNode node = new TreeNode(slow.val);
+        node.left = buildTree(head, pre);
+        node.right = buildTree(slow.next, tail);
+        return node;
+    }
+
+    // 130. 被围绕的区域
+    void join(int[] set, int a, int b) {
+        int root1 = find(set, a), root2 = find(set, b);
+        if (root1 != root2)
+            set[root2] = root1;
+    }
+
+    int find(int[] set, int a) {
+        int root = a;
+        while (root != set[root]) {
+            root = set[root];
+        }
+        int tmp = a;
+        while (tmp != set[tmp]) {
+            int b = set[tmp];
+            set[tmp] = root;
+            tmp = b;
+        }
+        return root;
+    }
+
+    public void solve(char[][] board) {
+        if (board.length == 0)
+            return;
+        int m = board.length, n = board[0].length;
+        int[] set = new int[m*n+1];
+        for (int i = 0; i < m*n+1; i++) {
+            set[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    int ind = i*n+j;
+                    if (i == 0 || i == m-1 || j == 0 || j == n-1)
+                        join(set, ind, m*n);
+                    if (j+1 < n && board[i][j+1] == 'O')
+                        join(set, ind, ind+1);
+                    if (i+1 < m && board[i+1][j] == 'O')
+                        join(set, ind, ind+n);
+                }
+            }
+        }
+
+        for (int i = 1; i < m-1; i++) {
+            for (int j = 1; j < n-1; j++) {
+                if (board[i][j] == 'O' && find(set, i*n+j) != find(set, m*n))
+                    board[i][j] = 'X';
+            }
+        }
+    }
+
+    // 133. 克隆图
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+    public Node cloneGraph(Node node) {
+        if (node == null)
+            return null;
+        HashMap<Integer, Node> hmap = new HashMap<>();
+        return dfs(hmap, node);
+    }
+
+    Node dfs(HashMap<Integer, Node> hmap, Node node) {
+        if (hmap.containsKey(node.val))
+            return hmap.get(node.val);
+        Node n = new Node(node.val);
+        hmap.put(node.val, n);
+        for (Node nd : node.neighbors) {
+            n.neighbors.add(dfs(hmap, nd));
+        }
+        return n;
+    }
+
+    // 200. 岛屿数量
+    public int numIslands(char[][] grid) {
+        int m = grid.length;
+        if (m == 0)
+            return 0;
+        int n = grid[0].length;
+        int num = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    num++;
+                    dfs200(grid, i, j, m, n);
+                }
+            }
+        }
+        return num;
+    }
+
+    void dfs200(char[][] grid, int i, int j, int m, int n) {
+        grid[i][j] = 'A';
+        if (j+1 < n && grid[i][j+1] == '1')
+            dfs200(grid, i, j+1, m, n);
+        if (i+1 < m && grid[i+1][j] == '1')
+            dfs200(grid, i+1, j, m, n);
+        if (j-1 >= 0 && grid[i][j-1] == '1')
+            dfs200(grid, i, j-1, m, n);
+        if (i-1 >= 0 && grid[i-1][j] == '1')
+            dfs200(grid, i-1, j, m, n);
+    }
+
+    // 210. 课程表 II
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] map = new int[numCourses];
+        ArrayList<LinkedList<Integer>> edges = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++)
+            edges.add(new LinkedList<>());
+        for (int[] tup : prerequisites) {
+            int course = tup[0];
+            map[course] += 1;
+            edges.get(tup[1]).add(course);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (map[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        int[] re = new int[numCourses];
+        int ind = 0;
+        while (!queue.isEmpty()) {
+            int cour = queue.poll();
+            re[ind++] = cour;
+
+            for (Integer after : edges.get(cour)) {
+                map[after] -= 1;
+                if (map[after] == 0) {
+                    queue.add(after);
+                }
+            }
+        }
+
+        if (ind != numCourses)
+            return new int[]{};
+        return re;
+    }
 }
--- src/com/isswhu/Main.java
+++ src/com/isswhu/Main.java
@@ -8,7 +8,8 @@ public class Main {
         //int[] arr = new int[]{0,1,1,1,4,5,3,7,7,8,10,2,7,8,0,5,2,16,12,1,19,15,5,18,2,2,22,15,8,22,17,6,22,6,22,26,32,8,10,11,2,26,9,12,9,7,28,33,20,7,2,17,44,3,52,27,2,23,19,56,56,58,36,31,1,19,19,6,65,49,27,63,29,1,69,47,56,61,40,43,10,71,60,66,42,44,10,12,83,69,73,2,65,93,92,47,35,39,13,75};
         int[] arr = new int[]{15252,16764,27963,7817,26155,20757,3478,22602,20404,6739,16790,10588,16521,6644,20880,15632,27078,25463,20124,15728,30042,16604,17223,4388,23646,32683,23688,12439,30630,3895,7926,22101,32406,21540,31799,3768,26679,21799,23740};
         Test t = new Test();
-        t.isPalindrome("0P");
+        char[][] cc = new char[][]{{'O','X','X','O','X'},{'X','O','O','X','O'},{'X','O','X','O','X'},{'O','X','O','O','O'},{'X','X','O','X','O'}};
+        t.findOrder(3, new int[][]{{0,1},{0,2},{1,2}});
     }
 
 
@@ -22,22 +23,46 @@ class ListNode {
 }
 
 class Test {
-    public boolean isPalindrome(String s) {
-        char[] chs = s.toCharArray();
-        int i = 0, j = s.length()-1;
-        while (i < j) {
-            while (!Character.isDigit(chs[i]) && !Character.isAlphabetic(chs[i]) && i < j)
-                i++;
-            while (!Character.isDigit(chs[j]) && !Character.isAlphabetic(chs[j]) && i < j)
-                j--;
-            if (i >= j)
-                return true;
-            if (chs[i] == chs[j] || Math.abs(chs[i]-chs[j]) == 'a' - 'A') {
-                i++; j--;
-                continue;
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        HashMap<Integer, LinkedList<Integer>> hmap = new HashMap<>();
+        for (int[] tup : prerequisites) {
+            int course = tup[0], pre = tup[1];
+            if (hmap.containsKey(course))
+                hmap.get(course).add(pre);
+            else {
+                hmap.put(course, new LinkedList<>());
             }
-            return false;
         }
-        return true;
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (!hmap.containsKey(i)) {
+                queue.add(i);
+            }
+        }
+
+        int[] re = new int[numCourses];
+        int ind = 0;
+        while (!queue.isEmpty()) {
+            int cour = queue.poll();
+            re[ind++] = cour;
+            LinkedList<Integer> removes = new LinkedList<>();
+            for (Map.Entry<Integer, LinkedList<Integer>> entry : hmap.entrySet()) {
+                entry.getValue().remove((Integer)cour);
+                if (entry.getValue().size() == 0) {
+                    queue.add(entry.getKey());
+                    removes.add(entry.getKey());
+                }
+            }
+
+            for (Integer i : removes)
+                hmap.remove(i);
+        }
+
+        if (ind != numCourses)
+            return new int[]{};
+        return re;
     }
+
+
 }
