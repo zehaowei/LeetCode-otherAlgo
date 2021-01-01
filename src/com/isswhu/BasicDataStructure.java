b0300f5f11359d0384e179e0627781e26a5629b1
zehaowei <314403865@qq.com>
2021-01-01 13:14:06 +0800

    [1-1] 3 problem

--- src/com/isswhu/BasicDataStructure.java
+++ src/com/isswhu/BasicDataStructure.java
@@ -379,29 +379,41 @@ public class BasicDataStructure {
     // 232. 用栈实现队列
     class MyQueue {
 
+        Stack<Integer> stk1;
+        Stack<Integer> stk2;
+
         /** Initialize your data structure here. */
         public MyQueue() {
-
+            stk1 = new Stack<>();
+            stk2 = new Stack<>();
         }
 
         /** Push element x to the back of queue. */
         public void push(int x) {
-
+            stk1.push(x);
         }
 
         /** Removes the element from in front of queue and returns that element. */
         public int pop() {
-
+            if (stk2.isEmpty()) {
+                while (!stk1.isEmpty())
+                    stk2.push(stk1.pop());
+            }
+            return stk2.pop();
         }
 
         /** Get the front element. */
         public int peek() {
-
+            if (stk2.isEmpty()) {
+                while (!stk1.isEmpty())
+                    stk2.push(stk1.pop());
+            }
+            return stk2.peek();
         }
 
         /** Returns whether the queue is empty. */
         public boolean empty() {
-
+            return stk1.isEmpty() && stk2.isEmpty();
         }
     }
 }
